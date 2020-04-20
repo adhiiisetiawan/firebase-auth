2ebba3acc5671553b795bdbbbf0fbd9edd0e0f51
adhiiisetiawan <adhisetiawan518@gmail.com>
2020-04-20 12:16:34 +0700

    add login feature

--- app/src/main/java/com/oxcart/firebasauthexample/MainActivity.java
+++ app/src/main/java/com/oxcart/firebasauthexample/MainActivity.java
@@ -1,16 +1,23 @@
 package com.oxcart.firebasauthexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private MaterialButton btnSignIn;
     private TextView textSignUp;
 
-    private ProgressBar progressBar;
+    private ProgressDialog progressDialog;
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +48,57 @@ public class MainActivity extends AppCompatActivity {
         btnSignIn = findViewById(R.id.button_sign_in);
         textSignUp = findViewById(R.id.text_dont_have_account);
 
-        progressBar = new ProgressBar(this);
+        progressDialog = new ProgressDialog(this);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = textEmail.getEditText().getText().toString();
+                password = textPassword.getEditText().getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+
+                    Toast.makeText(MainActivity.this,"Please enter email", Toast.LENGTH_LONG).show();
+
+                }
+                else if(TextUtils.isEmpty(password)){
+                    Toast.makeText(MainActivity.this,"Please enter password",Toast.LENGTH_LONG).show();
+
+                }
+                else if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+
+                    userLogin(email,password);
+                }
+            }
+        });
 
         textSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
                 finish();
+            }
+        });
+    }
+
+    private void userLogin(String email, String password){
+        progressDialog.setMessage("Loging in Please Wait...");
+        progressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                //if the task is successfull
+                if (task.isSuccessful()) {
+                    //start the profile activity
+                    finish();
+                    startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                }
             }
         });
     }
